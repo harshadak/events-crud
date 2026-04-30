7256195ecd5cebefd25338fddaa91560341f42eb
Harshada Kurahatti <harshada.kurahatti@gmail.com>
2026-04-30 13:06:01 -0700

    Refactor Events component to use React context instead of local state

--- frontend/src/Events.tsx
+++ frontend/src/Events.tsx
@@ -1,33 +1,44 @@
 import { useState, useEffect } from 'react';
+import { useEventsContext } from './hooks/EventHooks';
 import { Link } from 'react-router-dom';
 import type { EventType } from './types/EventType';
 
 function Events() {
-    const [events, setEvents] = useState<EventType[]>([]);
+    const { events } = useEventsContext();
+    const [filteredEvents, setFilteredEvents] = useState<EventType[]>([]);
     const [locations, setLocations] = useState<string[]>([]);
     const [filteredLocation, setFilteredLocation] = useState<string>("");
     const [keyword, setKeyword] = useState<string>("");
     const [debouncedKeyword, setDebouncedKeyword] = useState(keyword);
 
-    useEffect(() => {
-        const fetchEvents = async () => {
-            const locationQuery = filteredLocation ? `?location=${encodeURIComponent(filteredLocation)}` : '';
-            const keywordQuery = debouncedKeyword ? `${locationQuery ? "&" : "?"}keyword=${encodeURIComponent(debouncedKeyword)}` : '';
+    // useEffect(() => {
+    //     const fetchEvents = async () => {
+    //         const locationQuery = filteredLocation ? `?location=${encodeURIComponent(filteredLocation)}` : '';
+    //         const keywordQuery = debouncedKeyword ? `${locationQuery ? "&" : "?"}keyword=${encodeURIComponent(debouncedKeyword)}` : '';
 
-            try {
-                const response = await fetch(`http://localhost:3000/events${locationQuery}${keywordQuery}`);
-                if (!response.ok) {
-                    throw new Error(`HTTP error! status: ${response.status}`);
-                }
-                const data = await response.json();
-                setEvents(data);
-            } catch (error) {
-                console.error('Error fetching events:', error);
-            }
-        }
-        fetchEvents();
+    //         try {
+    //             const response = await fetch(`http://localhost:3000/events${locationQuery}${keywordQuery}`);
+    //             if (!response.ok) {
+    //                 throw new Error(`HTTP error! status: ${response.status}`);
+    //             }
+    //             const data = await response.json();
+    //             setEvents(data);
+    //         } catch (error) {
+    //             console.error('Error fetching events:', error);
+    //         }
+    //     }
+    //     fetchEvents();
+    //     findLocations();
+    // }, [filteredLocation, debouncedKeyword]);
+
+    useEffect(() => {
         findLocations();
-    }, [filteredLocation, debouncedKeyword]);
+    }, [debouncedKeyword]);
+
+    useEffect(() => {
+        setFilteredEvents([...events]);
+        findLocations();
+    }, [events]);
 
     useEffect(() => {
         const timer = setTimeout(() => {
@@ -40,13 +51,13 @@ function Events() {
     const findLocations = async () => {
 
         try {
-            const response = await fetch(`http://localhost:3000/events`);
-            if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
-            }
-            const data = await response.json();
+            // const response = await fetch(`http://localhost:3000/events`);
+            // if (!response.ok) {
+            //     throw new Error(`HTTP error! status: ${response.status}`);
+            // }
+            // const data = await response.json();
             const seen = new Set();
-            data.map((event: EventType) => {
+            filteredEvents.map((event: EventType) => {
                 if (!seen.has(event.location)) {
                     seen.add(event.location);
                     return event.location;
@@ -58,34 +69,47 @@ function Events() {
         }
     }
 
-    const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-        console.log(e.target.value);
-        setFilteredLocation(e.target.value);
+    const handleKeywordChange = (e: React.ChangeEvent<HTMLInputElement, HTMLInputElement>) => {
+        setKeyword(e.target.value);
+        filteredResults(e.target.value, filteredLocation);
     }
 
-    // console.log('keyword = ', keyword);
-    // console.log('debouncedKeyword = ', debouncedKeyword);
+    const handleLocationChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        setFilteredLocation(e.target.value);
+        filteredResults(debouncedKeyword, e.target.value);
+    }
+
+    const filteredResults = (keyword: string, location: string) => {
+        const filteredEvents = events.filter(event => {
+            const matchesLocation = !location || event.location === location;
+            const matchesKeyword = !keyword || event.title.toLowerCase().includes(keyword.toLowerCase());
+
+            return matchesLocation && matchesKeyword;
+        });
+
+        setFilteredEvents(filteredEvents);
+    }
 
     return (
         <>
             {/* Text based search */}
-            <input type='text' value={keyword} onChange={(e) => setKeyword(e.target.value)} placeholder='Search by keyword' />
+            <input type='text' value={keyword} onChange={(e) => handleKeywordChange(e)} placeholder='Search by keyword' />
             {/* Filter by location: */}
             <label htmlFor="location-filter" style={{ marginRight: 10 }}>Filter by location:</label>
-            <select name="" id="" onChange={(e) => handleChange(e)} value={filteredLocation}>
+            <select name="" id="" onChange={(e) => handleLocationChange(e)} value={filteredLocation}>
                 <option value="">All</option>
                 {locations.map(location => (
                     <option key={location} value={location}>{location}</option>
                 ))}
             </select>
 
-            {events.map(event => (
+            {filteredEvents.map(event => (
                 <div key={event.id} className='event-card'>
                     <Link to={`/events/${event.id}`}><h3>{event.title}</h3></Link>
                 </div>
             ))}
 
-            {events.length === 0 && <div style={{ marginTop: 24}}>No events matching the filters! Please try again.</div>}
+            {filteredEvents.length === 0 && <div style={{ marginTop: 24 }}>No events matching the filters! Please try again.</div>}
         </>
     );
 }
